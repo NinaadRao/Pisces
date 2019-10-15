e2898d763a85067417ae49b88b8ae001e8b6e6c2
midsterx <mimosk25@gmail.com>
2019-10-16 00:41:27 +0530

    Restructured the directory and added login UI

---
+++ pisces/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/midsk/Documents/College/Sem 7/Software Engineering (SE)/Pisces/pisces/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
