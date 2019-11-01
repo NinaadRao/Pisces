5d1cd1d0c6eb88ef750fbbe85af193987710309b
NinaadRao <ninaadrrao@gmail.com>
2019-11-01 16:56:43 +0530

    Company display done.

--- pisces/pisces/settings.py
+++ pisces/pisces/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'accounts','students',
     'rest_framework', 'rest_framework_mongoengine'
 ]
 
