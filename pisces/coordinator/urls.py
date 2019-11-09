978baeb44a9a9fbf8ac50ca4ca91caac25c880a0
Nishant <nishantravishankar@gmail.com>
2019-11-09 10:00:07 +0530

    random changes

--- pisces/coordinator/urls.py
+++ pisces/coordinator/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r"^schedule$", DateView.as_view(), name="schedule"),
-    url(r"^lab-list$", LabListView.as_view(), name="lab-list")
+    url(r"^lab-list$", LabListView.as_view(), name="lab-list"),
+    url(r"^booking$", BookView.as_view(), name="booking")
 ]
