c1a22ca76ee6808b3c8454fdc0fc81c074ab4239
NinaadRao <ninaadrrao@gmail.com>
2019-11-09 15:33:09 +0530

    Registration, view, upload resume

--- pisces/students/urls.py
+++ pisces/students/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url(r"^homepage",HomePage.as_view(),name="homepage"),
+    url(r"^homepage", HomePage.as_view(), name="homepage"),
     url(r"^logout$", logout.as_view(), name='logout'),
-    url(r"^update",updatePassword.as_view(),name="updatePassword"),
-    url(r"^company",Company_list.as_view(),name="company_list")
+    url(r"^update", updatePassword.as_view(), name="updatePassword"),
+    url(r"^company", Company_list.as_view(), name="company_list"),
+    url(r"^register", register.as_view(), name="register_company"),
+    url(r"^uploadResume", uploadResume.as_view(), name="upload_resume"),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
