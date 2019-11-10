b948207792053e76026e924859002db3778f5d33
Nishant <nishantravishankar@gmail.com>
2019-11-11 00:09:35 +0530

    Added new apis, refactored code and added UI

--- pisces/coordinator/urls.py
+++ pisces/coordinator/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r"^schedule$", LabListView.as_view(), name="schedule")
-
+    url(r"^schedule$", LabListView.as_view(), name="schedule"),
+    url(r"^list_labs$", ListLabs.as_view(), name="list_labs"),
+    url(r"^upsert_booking$", UpsertBooking.as_view(), name="upsert_booking"),
+    url(r"^remove_booking$", RemoveBooking.as_view(), name="remove_booking")
 ]
