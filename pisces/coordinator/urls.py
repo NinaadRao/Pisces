5a08766910ff9ec9518ea13ea7e5062580750e3b
Nishant <nishantravishankar@gmail.com>
2019-11-12 13:46:23 +0530

    Added deselect to scheduling UI and added new ui

--- pisces/coordinator/urls.py
+++ pisces/coordinator/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r"^search$", Search.as_view(), name="search"),
     url(r"^schedule$", LabListView.as_view(), name="schedule"),
     url(r"^list_labs$", ListLabs.as_view(), name="list_labs"),
+    url(r"^get_booking$", DisplayBooking.as_view(), name="get_booking"),
     url(r"^upsert_booking$", UpsertBooking.as_view(), name="upsert_booking"),
     url(r"^remove_booking$", RemoveBooking.as_view(), name="remove_booking")
 ]
